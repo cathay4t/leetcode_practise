ffdc121509aaa108de35f5e4d591cb557615af39
Gris Ge <cnfourt@gmail.com>
2026-04-30 15:42:27 +0800

    Finished 560
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- h560_subarray_sum/src/main.rs
+++ h560_subarray_sum/src/main.rs
@@ -4,19 +4,29 @@ fn main() {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
         use std::collections::HashMap;
 
-        // contains <pre_sum, count>
-        let mut pre_sums: HashMap<i32, i32> = HashMap::new();
+        // we need to find nums[i, j] (both inclusive) subarray with sum of k.
+        // define `pre_sums[i]` means the sum of numbers before(include) i.
+        // So k = pre_sums[j] - pre_sums[i - 1]
+        // For each `pre_sums[j]`, we need to find out how many count
+        // of sum equal to `presum[i-1] - k`
+
+        // `HashMap<pre_sum, count>`
+        let mut pre_sums = HashMap::new();
+        // empty array sums 0, so it always has 1 count
+        pre_sums.insert(0, 1);
 
         let mut pre_sum = 0;
-        for i in 0..nums.len() {
-            pre_sum += nums[i];
-            pre_sums.entry(pre_sum).and_modify(|i| *i +=1).or_insert(1);
+        let mut count = 0;
+
+        for j in 0..nums.len() {
+            pre_sum += nums[j];
+
+            if let Some(c) = pre_sums.get(&(pre_sum - k)) {
+                count += *c;
+            }
+            *(pre_sums.entry(pre_sum).or_insert(0)) += 1;
         }
-
-        println!("HAHA685 {:?}",  pre_sums);
-
-        0
-
+        count
     }
 
     assert_eq!(subarray_sum(vec![1i32, 1, 1], 2), 2);
